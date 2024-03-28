b71020dfb75274aa489f3f7562cb092fdf68d272
Mikeeyh <133797823+Mikeeyh@users.noreply.github.com>
2024-03-28 17:15:09 +0200

    add

--- Speechify/voices/urls.py
+++ Speechify/voices/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 from Speechify.voices.views import AudioFileCreateView, AudioFileDetailView, AudioFileUpdateView, \
     AudioFileDeleteView, AudioFileListView
 
